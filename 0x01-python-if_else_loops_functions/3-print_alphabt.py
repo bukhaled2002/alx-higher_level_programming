ab4a61735449aa38dde2b4b9a744369a9dbe7956
bukhaled2002 <bakhaled.310@gmail.com>
2023-08-11 11:04:26 -0700

    ff

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 # 3-print_alphabet.py
 for x in range(97, 123):
-    if chr(x) != 'q' or chr(x) != 'e':
+    if chr(x) != 'q' and chr(x) != 'e':
         print("{}".format(chr(x)), end="")
