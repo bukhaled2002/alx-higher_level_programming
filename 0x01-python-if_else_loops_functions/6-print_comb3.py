3284a79fd27fc40da1cd6abf198e773f1241c9a3
bukhaled2002 <bakhaled.310@gmail.com>
2023-08-12 03:23:59 -0700

    d

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -3,6 +3,6 @@ for digit1 in range(0, 10):
     for digit2 in range(digit1, 10):
         if f"{digit1}{digit2}" != f"{digit2}{digit1}":
             if f"{digit1}{digit2}" != "89":
-                print("{}".format(int(f"{digit1}{digit2}"), end=", "))
+                print("{}".format(int(f"{digit1}{digit2}")), end=", ")
             else:
-                print("{}".format(int(f"{digit1}{digit2}")))
+                print("{:0>2d}".format(int(f"{digit1}{digit2}")))
