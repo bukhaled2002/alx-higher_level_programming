7f28fd2805f76d7cac0c729b176f7a1f81a3f2a1
bukhaled2002 <bakhaled.310@gmail.com>
2023-08-12 04:02:47 -0700

    d

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -3,8 +3,6 @@ def uppercase(str):
     difference = ord('A')-ord('a')
     for letter in str:
         if ord(letter) >= ord('a') and ord(letter) <= ord('z'):
-            print("{}".format(chr(ord(letter) + difference)), end="")
-        else:
-            print("{}".format(chr(ord(letter))), end="")
-        if letter == str[-1]:
-            print("")
+            letter = chr(ord(letter) + difference)
+        print("{}".format(letter), end="")
+    print("")
