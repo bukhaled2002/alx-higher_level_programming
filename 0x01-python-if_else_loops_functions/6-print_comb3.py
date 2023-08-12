94368eb6d523dd06de7ca5b168dc05d5410b3dfb
bukhaled2002 <bakhaled.310@gmail.com>
2023-08-12 03:22:39 -0700

    d

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python3
-lis = []
-for digit1 in range(0,10):
-    for digit2 in range(digit1,10):
+for digit1 in range(0, 10):
+    for digit2 in range(digit1, 10):
         if f"{digit1}{digit2}" != f"{digit2}{digit1}":
-            lis.append(int(f"{digit1}{digit2}"))
-for x in lis:
-    if x != lis[-1]:
-        print("{:0>2d}".format(x),end=", ")
-    else:
-        print("{:0>2d}".format(x))
+            if f"{digit1}{digit2}" != "89":
+                print("{}".format(int(f"{digit1}{digit2}"), end=", "))
+            else:
+                print("{}".format(int(f"{digit1}{digit2}")))
