2934c4c6641f241f66f410023c3c72017da384df
bukhaled2002 <bakhaled.310@gmail.com>
2023-08-14 05:42:36 -0700

    f

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def replace_in_list(my_list, idx, element):
+    prototype=my_list[:]
+    if idx < 0:
+        return prototype
+    if idx > len(my_list) - 1:
+        return prototype
+    prototype[idx] = element
+    return prototype
+
