7a81c22b6b8ccf6dabecc127845fe1fd0834b8ed
bukhaled2002 <bakhaled.310@gmail.com>
2023-08-14 05:56:08 -0700

    f

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
     prototype=my_list[:]
-    if idx < 0:
-        return prototype
-    if idx > len(my_list) - 1:
-        return prototype
+    if idx < 0 or idx > len(my_list) - 1:
+        return (my_list)
     prototype[idx] = element
-    return prototype
+    return (prototype)
