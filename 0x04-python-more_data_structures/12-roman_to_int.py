61af0c4f660974dc3a021e179cf658f509ae55ca
bukhaled2002 <bakhaled.310@gmail.com>
2023-08-16 08:40:52 -0700

    f

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def roman_to_int(roman_string:str):
+    roman_switch={'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000}
+    n=0
+    if len(roman_string) <=1 : return roman_switch.get(roman_string[0])
+    for i in range(len(roman_string)-1):
+        if roman_switch[roman_string[i]] >= roman_switch[roman_string[i+1]]:
+            n += roman_switch[roman_string[i]]
+        else:
+            n -= roman_switch[roman_string[i]]
+    n += roman_switch[roman_string[-1]]
+    return n
