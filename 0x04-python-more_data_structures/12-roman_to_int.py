cfcbab82ec157e1c63cd13bb9408502e3cdd3ddb
bukhaled2002 <bakhaled.310@gmail.com>
2023-08-16 08:52:04 -0700

    f

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python3
 
-def roman_to_int(roman_string:str):
-    roman_switch={'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000}
-    n=0
+def roman_to_int(roman_string: str):
+    roman_switch = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
+                    'C': 100, 'D': 500, 'M': 1000}
+    n = 0
     if not roman_string:
-        return None
-    if len(roman_string) <= 1 : return roman_switch.get(roman_string[0])
+        return 0
+    if not isinstance(roman_string, str):
+        return 0
+    if len(roman_string) <= 1:
+        return roman_switch.get(roman_string[0])
     for i in range(len(roman_string) - 1):
         if roman_switch[roman_string[i]] >= roman_switch[roman_string[i + 1]]:
             n += roman_switch[roman_string[i]]
