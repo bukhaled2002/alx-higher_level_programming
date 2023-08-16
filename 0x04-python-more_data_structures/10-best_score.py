893b731090e430f092d92ba4397e163f1d2e39da
bukhaled2002 <bakhaled.310@gmail.com>
2023-08-16 04:12:11 -0700

    ff

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -3,15 +3,15 @@
 def best_score(a_dictionary: dict):
     if a_dictionary == None:
         return None
-    key=list(a_dictionary.keys())
+    key = list(a_dictionary.keys())
     if len(key) < 1:
         return None
     print(key)
     large=0
-    value =''
+    value = ''
     for i in key:
-        if a_dictionary[i]>large:
+        if a_dictionary[i] >= large:
             value = i
-            large=a_dictionary[i]
+            large = a_dictionary[i]
         else: continue
     return value
