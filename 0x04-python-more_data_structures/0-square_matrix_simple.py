a557e8773ea0de9a5ce91dd4b6cc47643d8f84e7
bukhaled2002 <bakhaled.310@gmail.com>
2023-08-16 02:10:48 -0700

    ff

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    matrix_cp=matrix[:]
+    matrix_cp = matrix[:]
     for i in range(len(matrix)):
-        matrix_cp[i]=list(map(lambda x : x*x,matrix[i]))
+        matrix_cp[i] = list(map(lambda x: x*x, matrix[i]))
     return matrix_cp
--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    list_cp= my_list[:]
-    list_cp= list(map(lambda x: replace if x == search else x, my_list))
+    list_cp = my_list[:]
+    list_cp = list(map(lambda x: replace if x == search else x, my_list))
     return list_cp
