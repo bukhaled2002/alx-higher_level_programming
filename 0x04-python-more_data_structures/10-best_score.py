dcc13f37b81222d006429474ce9113221c767c71
bukhaled2002 <bakhaled.310@gmail.com>
2023-08-16 04:11:21 -0700

    ff

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+#!/usr/bin/python3
+def best_score(a_dictionary: dict):
+    if a_dictionary == None:
+        return None
+    key=list(a_dictionary.keys())
+    if len(key) < 1:
+        return None
+    print(key)
+    large=0
+    value =''
+    for i in key:
+        if a_dictionary[i]>large:
+            value = i
+            large=a_dictionary[i]
+        else: continue
+    return value
