24ae2b6a8685e479c96897e47bdcec51c195f1a5
bukhaled2002 <bakhaled.310@gmail.com>
2023-08-16 02:29:32 -0700

    ff

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,10 +1,4 @@
 #!/usr/bin/python3
 def only_diff_elements(set_1, set_2):
-    result = []
-    for i in set_1:
-        for j in set_2:
-            if i != j:
-                result.append(i)
-            else:
-                continue
-    return set(result)
+    result = set_1.symmetric_difference(set_2)
+    return result
