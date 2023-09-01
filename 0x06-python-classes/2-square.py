fdaf1af1e237f54e2418c1574b28da3859de1116
bukhaled2002 <bakhaled.310@gmail.com>
2023-09-01 02:40:50 -0700

    ff

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -3,11 +3,11 @@
 
 
 class Square:
-
     """Square class with no attributes"""
+
     def __init__(self, size=0):
         if not size.isdigit():
             raise TypeError("size must be an integer")
-        if size < 0:
+        elif size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
