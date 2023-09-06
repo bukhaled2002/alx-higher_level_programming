913f04627566c1e1cb10ec024ab7db544c941915
bukhaled2002 <bakhaled.310@gmail.com>
2023-09-06 14:46:16 -0700

    ff

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -21,7 +21,7 @@ class Rectangle:
 
     @classmethod
     def square(cls, size=0):
-        return(cls(size, size))
+        return (cls(size, size))
 
     def __init__(self, width=0, height=0):
         """Initialize a new Rectangle.
