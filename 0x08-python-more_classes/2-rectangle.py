a6dd4cd86e6c3d71e2dd2ac29c207446d8c7d0e6
bukhaled2002 <bakhaled.310@gmail.com>
2023-09-06 12:36:28 -0700

    dd

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -51,4 +51,6 @@ class Rectangle:
         """
         Return perimeter
         """
+        if self.__height == 0 or slef.__width == 0:
+            return 0
         return (self.__height + self.__width) * 2
