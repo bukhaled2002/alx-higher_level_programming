d1bf94cd56d869230fe40b6715a66ac2459ff514
bukhaled2002 <bakhaled.310@gmail.com>
2023-09-06 12:37:18 -0700

    dd

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -53,4 +53,5 @@ class Rectangle:
         """
         if self.__height == 0 or slef.__width == 0:
             return 0
-        return (self.__height + self.__width) * 2
+        else
+            return (self.__height + self.__width) * 2
