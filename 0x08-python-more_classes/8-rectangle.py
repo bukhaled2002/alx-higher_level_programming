8ed54dfbe310c5178a3bfbd1d8a34bb14bff221b
bukhaled2002 <bakhaled.310@gmail.com>
2023-09-06 14:38:15 -0700

    ff

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -16,7 +16,7 @@ class Rectangle:
             raise TypeError("rect_2 must be an instance of Rectangle")
         if rect_1.area() >= rect_2.area():
             return rect_1
-        if rect_2.area() > rect_1.area():             
+        if rect_2.area() > rect_1.area():
             return rect_2
 
     def __init__(self, width=0, height=0):
