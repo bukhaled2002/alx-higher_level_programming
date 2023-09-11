17e582d2687a7cbde8ebf95036c981eb804be3ac
bukhaled2002 <bakhaled.310@gmail.com>
2023-09-11 05:23:15 -0700

    ff

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -12,6 +12,6 @@ def inherits_from(obj, a_class):
         If obj is exactly an instance of a_class - True.
         Otherwise - False.
     """
-    if issubclass(obj, a_class):
+    if issubclass(type(obj), a_class) and type(obj) != a_class:
         return True
     return False
