f444a5b7570822329816af6f19e5d18d44056a9e
bukhaled2002 <bakhaled.310@gmail.com>
2023-09-13 14:50:38 -0700

    f

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -10,8 +10,8 @@ class Student:
         self.age = age
 
     def to_json(self, attrs = None):
-        if type(attrs) == list and all(type(elem) == str for elem in attrs):
-            return {k:getattr(self, k) for k in attrs if hasattr(self, k)}
+        if isinstance(attrs, list) and all(type(elem) == str for elem in attrs):
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
         return self.__dict__
 
     def reload_from_json(self, json):
