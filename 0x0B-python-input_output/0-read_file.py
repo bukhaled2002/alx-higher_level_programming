388471684ac55c8e60154a5426b747606b7620a4
bukhaled2002 <bakhaled.310@gmail.com>
2023-09-13 04:46:49 -0700

    f

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -6,4 +6,4 @@ def read_file(filename=""):
     """read a text file and print it"""
     with open(filename, encoding="utf-8") as file:
         for line in file:
-            print(line, end="");
+            print(line, end="")
