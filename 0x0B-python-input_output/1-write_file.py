18444576681572867137dc15961852e82be0d1a0
bukhaled2002 <bakhaled.310@gmail.com>
2023-09-13 05:02:18 -0700

    f

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -4,6 +4,6 @@
 
 def write_file(filename="", text=""):
     """read a text file and print it"""
-    with open(filename, 'w', encoding="utf-8") as file:
-        file.write(text)
+    with open(filename, mode='w', encoding="utf-8") as file:
+        return file.write(text)
         
